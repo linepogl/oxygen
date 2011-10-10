59270f5e2b6f62f31a787cb679f2aa26d246b24e
Lazaros Inepologlou <li@tcpartners.fr>
2011-10-10 17:08:46 +0200

    profiler

--- src/Utils/Profiler.php
+++ src/Utils/Profiler.php
@@ -5,7 +5,7 @@ class Profiler {
 
 	public static function Start(){
 		if (!function_exists('xhprof_enable')) return;
-		xhprof_enable();
+		xhprof_enable(XHPROF_FLAGS_NO_BUILTINS);
 	}
 	
 	public static function Stop(){
@@ -44,7 +44,6 @@ class Profiler {
 		foreach ($results as $key=>$a){
 			$f = explode('==>',$key);
 			$ff = explode('@',$f[0]); $fo = $ff[0];
-
 			if (!array_key_exists('',$total_calls[$fo])) {
 				$total_calls[$fo][''] = 0;
 				$direct_calls[$fo][''] = 0;
@@ -60,13 +59,10 @@ class Profiler {
 			elseif (count($f) == 2) {
 				$exclusive_time[$fo][''] -= $a['wt'];
 			}
-
 			for ($i = 0; $i < count($f) - 1; $i++){
 				$ff = explode('@',$f[$i]); $fi = $ff[0];
-
 				for ($j = $i+1; $j < count($f); $j++){
 					$ff = explode('@',$f[$j]); $fj = $ff[0];
-
 					if (!array_key_exists($fi,$total_calls[$fj])) {
 						$total_calls[$fj][$fi] = 0;
 						$direct_calls[$fj][$fi] = 0;
@@ -88,7 +84,6 @@ class Profiler {
 					}
 				}
 			}
-
 		}
 
 
@@ -104,12 +99,13 @@ class Profiler {
 
 		$b = array();
 		$b[0] = array_sum(array_map( function($x){ return $x[0]; }, $a));
-		$b[1] = array_sum(array_map( function($x){ return $x[1]; }, $a));
+		$b[1] = $total_time['main()'][''];
 		$b[2] = array_sum(array_map( function($x){ return $x[2]; }, $a));
 
 
 		echo '<style>';
 		echo 'table.system td, table.system th { font:11px/12px monospace; padding:2px 24px 2px 4px; border-bottom:1px solid #cccccc; color:#333333; }';
+		echo 'table.system .alt { background:#eeeeee; }';
 		echo '</style>';
 
 		echo '<div style="position:fixed;top:30px;left:30px;right:30px;bottom:30px;;overflow:auto;z-index:10000;white-space:pre;background:#f4f4f4;border:6px solid #333333;padding:10px;">';
@@ -123,15 +119,15 @@ class Profiler {
 
 		$i = 0;
 		foreach ($a as $key=>$aa){
-			if ($i++>100) break;
-			echo '<tr>';
+			//if ($i++>100) break;
+			echo '<tr'.($i%2==0?' class="alt"':'').'>';
 			echo '<th class="hleft">'.$key.'</th>';
 			echo '<td class="hright">'.$aa[0].'</td>';
-			echo '<td class="hright">'.sprintf('%.3f',$aa[0]/$b[0]*100).'%</td>';
+			echo '<td class="hright">'.sprintf('%.2f',$aa[0]/$b[0]*100).'%</td>';
 			echo '<td class="hright">'.$aa[1].'</td>';
-			echo '<td class="hright">'.sprintf('%.3f',$aa[1]/$b[1]*100).'%</td>';
+			echo '<td class="hright">'.sprintf('%.2f',$aa[1]/$b[1]*100).'%</td>';
 			echo '<td class="hright">'.$aa[2].'</td>';
-			echo '<td class="hright">'.sprintf('%.3f',$aa[2]/$b[2]*100).'%</td>';
+			echo '<td class="hright">'.sprintf('%.2f',$aa[2]/$b[2]*100).'%</td>';
 			echo '</tr>';
 		}
 
