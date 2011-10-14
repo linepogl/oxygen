72c132893d7afa1f04df09c01ee7d558c8dbde6d
Lazaros Inepologlou <li@tcpartners.fr>
2011-10-14 17:15:34 +0200

    correction

--- src/OmniType/Converters/Import/XmlValue.php
+++ src/OmniType/Converters/Import/XmlValue.php
@@ -15,25 +15,24 @@ class XmlValue extends ImportConverter {
 
 
 
-	public function AsString(){ return is_null($this->value) ? null : strval($this->value); }
-	public function AsID(){ return is_null($this->value) ? null : new ID(strval($this->value)); }
-  public function AsDateTime(){
-  	return empty($this->value) ? null : XDateTime::Parse($this->value,'Y-m-d\TH:i:s');
-  }
-  public function AsDate(){
-  	return empty($this->value) ? null : XDate::Parse($this->value,'Y-m-d');
-  }
-  public function AsTime(){
-  	return empty($this->value) ? null : XTime::Parse($this->value,'H:i:s');
-  }
-  public function AsTimeSpan(){
-  	return empty($this->value) ? null : XTimeSpan::Parse($this->value);
-	}
+//	public function AsString(){ return is_null($this->value) ? null : strval($this->value); }
+//	public function AsID(){ return is_null($this->value) ? null : new ID(strval($this->value)); }
+//  public function AsDateTime(){
+//  	return empty($this->value) ? null : XDateTime::Parse($this->value,'Y-m-d\TH:i:s');
+//  }
+//  public function AsDate(){
+//  	return empty($this->value) ? null : XDate::Parse($this->value,'Y-m-d');
+//  }
+//  public function AsTime(){
+//  	return empty($this->value) ? null : XTime::Parse($this->value,'H:i:s');
+//  }
+//  public function AsTimeSpan(){
+//  	return empty($this->value) ? null : XTimeSpan::Parse($this->value);
+//	}
 
-//	public function AsBoolean(){ return OmniBoolean::ImportDomValue($this->value); }
-  public function AsBoolean(){ return is_null($this->value) ? false : $this->value=='true'; }
-  public function AsInteger(){ return is_null($this->value) ? 0 : intval($this->value); }
-  public function AsFloat(){ return is_null($this->value) ? 0.0 : floatval($this->value); }
+//  public function AsBoolean(){ return is_null($this->value) ? false : $this->value=='true'; }
+//  public function AsInteger(){ return is_null($this->value) ? 0 : intval($this->value); }
+//  public function AsFloat(){ return is_null($this->value) ? 0.0 : floatval($this->value); }
 
 
 	public function CastTo(OmniType $type) {
