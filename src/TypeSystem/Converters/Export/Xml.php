505c6d7f52279b9d74012d3c3021e5acfca3198e
Lazare Inepologlou <li@tcpartners.fr>
2012-03-27 18:04:09 +0200

    xml header

--- src/TypeSystem/Converters/Export/Xml.php
+++ src/TypeSystem/Converters/Export/Xml.php
@@ -11,6 +11,7 @@ class Xml extends ExportConverter {
 	const Attribute = 1;
 	const Element = 2;
 
+	const HEADER = '<?xml version="1.0" encoding="UTF-8"?>';
 	const NS = 'http://www.w3.org/2000/xmlns/';
 	const XS = 'http://www.w3.org/2001/XMLSchema';
 
