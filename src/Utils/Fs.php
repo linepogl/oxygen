871d6aed6f1a73300128c5911b5c73e777a166c9
Lazare Inepologlou <li@tcpartners.fr>
2012-10-23 14:28:04 +0200

    m4v mime type

--- src/Utils/Fs.php
+++ src/Utils/Fs.php
@@ -149,6 +149,7 @@ class Fs {
 			case 'asx': return 'video/x-ms-asf';
 			case 'avi': return 'video/avi';
 			case 'mp4': return 'video/mp4';
+			case 'm4v': return 'video/x-m4v';
 			case 'mpg': return 'video/mpeg';
 			case 'mpeg': return 'video/mpeg';
 			case 'wmv': return 'video/x-ms-wmv';
