5ff1717209fecb40694fabc4d66711d39a9a99b0
Lazare Inepologlou <li@tcpartners.fr>
2012-11-08 15:49:55 +0100

    DISABLE IGBINARY!

--- _.php
+++ _.php
@@ -10,7 +10,8 @@ define('DEBUG',array_key_exists('debug',$_GET));
 define('PROFILE',array_key_exists('profile',$_GET));
 define('DEV',!isset($_SERVER["SERVER_NAME"]) || $_SERVER["SERVER_NAME"] == 'localhost');
 
-define('IS_IGBINARY_AVAILABLE',function_exists('igbinary_serialize'));
+//define('IS_IGBINARY_AVAILABLE',function_exists('igbinary_serialize'));
+define('IS_IGBINARY_AVAILABLE',false);
 define('IS_APC_AVAILABLE',function_exists('apc_exists'));
 define('IS_MEMCACHED_AVAILABLE',class_exists('Memcached'));
 
