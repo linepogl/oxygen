774fd2b0579db74bd6da1ea527166f3839aafcb8
Lazaros Inepologlou <li@tcpartners.fr>
2013-01-11 13:08:01 +0100

    profiler

--- src/Utils/Profiler.php
+++ src/Utils/Profiler.php
@@ -31,81 +31,44 @@ class Profiler {
 
 
 	public static function Analyse($results){
-		$total_calls = array();
-		$direct_calls = array();
-		$total_time = array();
-		$exclusive_time = array();
 
-		foreach ($results as $key=>$a){
-			$f = explode('==>',$key);
-			$ff = explode('@',$f[count($f)-1]);
-			$fz = $ff[0];
-			$total_calls[$fz] = array();
-			$direct_calls[$fz] = array();
-			$total_time[$fz] = array();
-			$exclusive_time[$fz] = array();
-		}
-		foreach ($results as $key=>$a){
-			$f = explode('==>',$key);
-			$ff = explode('@',$f[0]); $fo = $ff[0];
-			if (!array_key_exists('',$total_calls[$fo])) {
-				$total_calls[$fo][''] = 0;
-				$direct_calls[$fo][''] = 0;
-				$total_time[$fo][''] = 0;
-				$exclusive_time[$fo][''] = 0;
+		$calls = array();
+		$inclu = array();
+		$exclu = array();
+
+		foreach ($results as $key=>$z){
+			$b = explode('==>',$key);
+			if (count($b) == 1){
+				$from = '';
+				$call = $b[ 0 ];
 			}
-			if (count($f) == 1){
-				$total_calls[$fo][''] += $a['ct'];
-				$direct_calls[$fo][''] += $a['ct'];
-				$total_time[$fo][''] += $a['wt'];
-				$exclusive_time[$fo][''] += $a['wt'];
+			else {
+				$from = $b[ 0 ];
+				$call = $b[ 1 ];
 			}
-			elseif (count($f) == 2) {
-				$exclusive_time[$fo][''] -= $a['wt'];
-			}
-			for ($i = 0; $i < count($f) - 1; $i++){
-				$ff = explode('@',$f[$i]); $fi = $ff[0];
-				for ($j = $i+1; $j < count($f); $j++){
-					$ff = explode('@',$f[$j]); $fj = $ff[0];
-					if (!array_key_exists($fi,$total_calls[$fj])) {
-						$total_calls[$fj][$fi] = 0;
-						$direct_calls[$fj][$fi] = 0;
-						$total_time[$fj][$fi] = 0;
-						$exclusive_time[$fj][$fi] = 0;
-					}
-					if ($i == count($f)-2 && $j == count($f)-1) {
-						$total_calls[$fj][$fi] += $a['ct'];
-						$direct_calls[$fj][$fi] += $a['ct'];
-						$total_time[$fj][$fi] += $a['wt'];
-						$exclusive_time[$fj][$fi] += $a['wt'];
-					}
-					elseif ($i == count($f)-3 && $j == count($f)-2) {
-						$exclusive_time[$fj][$fi] -= $a['wt'];
-					}
-					elseif ($j == count($f)-1) {
-						$total_time[$fj][$fi] += $a['wt'];
-						$total_calls[$fj][$fi] += $a['ct'];
-					}
-				}
+
+			list($from) = explode('@',$from);
+			list($call) = explode('@',$call);
+
+			if (!array_key_exists($call,$calls)) $calls[$call] = 0;
+			$calls[$call] += $z['ct'];
+
+			if (!array_key_exists($call,$inclu)) $inclu[$call] = 0;
+			$inclu[$call] += $z['wt'];
+
+			if (!array_key_exists($call,$exclu)) $exclu[$call] = 0;
+			$exclu[$call] += $z['wt'];
+
+			if ($from !== '') {
+				if (!array_key_exists($from,$exclu)) $exclu[$from] = 0;
+				$exclu[$from] -= $z['wt'];
 			}
 		}
 
+		arsort($exclu);
 
-		$a = array();
-		foreach ($total_calls as $key=>$value){
-			if (array_key_exists('',$total_calls[$key]))
-				$a[$key] = array($total_calls[$key][''],$total_time[$key][''],array_sum($exclusive_time[$key]));
-			else
-				$a[$key] = array($total_calls[$key]['main()'],$total_time[$key]['main()'],array_sum($exclusive_time[$key]));
-		}
-
-		uasort($a,function($x1,$x2){return $x2[2]-$x1[2]; });
-
-		$b = array();
-		$b[0] = array_sum(array_map( function($x){ return $x[0]; }, $a));
-		$b[1] = $total_time['main()'][''];
-		$b[2] = array_sum(array_map( function($x){ return $x[2]; }, $a));
-
+		$t_calls = array_sum($calls);
+		$t_exclu = array_sum($exclu);
 
 
     echo '<table class="console" width="100%" cellpadding="0" cellspacing="0" border="0">';
@@ -117,18 +80,24 @@ class Profiler {
     echo '</tr>';
 
 		$i = 0;
-		foreach ($a as $key=>$aa){
-			//if ($i++>100) break;
-			echo '<tr'.(++$i%2==0?' class="alt"':'').'>';
+		foreach ($exclu as $key=>$v){
+			if ($i++>60) break;
+			echo '<tr'.($i%2==0?' class="alt"':'').'>';
 			echo '<td class="hleft">'.$key.'</td>';
-			echo '<td class="hright">'.$aa[0].'</td>';
-			echo '<td class="hright">'.sprintf('%.2f',$aa[0]/$b[0]*100).'%</td>';
-			echo '<td class="hright">'.$aa[1].'</td>';
-			echo '<td class="hright">'.sprintf('%.2f',$aa[1]/$b[1]*100).'%</td>';
-			echo '<td class="hright">'.$aa[2].'</td>';
-			echo '<td class="hright">'.sprintf('%.2f',$aa[2]/$b[2]*100).'%</td>';
+			echo '<td class="hright">'.$calls[$key].'</td>';
+			echo '<td class="hright">'.sprintf('%.2F',$calls[$key]/$t_calls*100).'%</td>';
+			echo '<td class="hright">'.$inclu[$key].'</td>';
+			echo '<td class="hright">'.sprintf('%.2F',$inclu[$key]/$t_exclu*100).'%</td>';
+			echo '<td class="hright">'.$exclu[$key].'</td>';
+			echo '<td class="hright">'.sprintf('%.2F',$exclu[$key]/$t_exclu*100).'%</td>';
 			echo '</tr>';
 		}
+
+		echo '<tr>';
+	  echo '<th class="expand" colspan="5">&nbsp;</th>';
+		echo '<th class="hright">'.$t_exclu.'</td>';
+		echo '<th class="hright">'.sprintf('%.2F',100).'%</td>';
+	  echo '</tr>';
 
 		echo '</table>';
 	}
