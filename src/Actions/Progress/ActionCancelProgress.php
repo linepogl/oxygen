f3898de44bb6f94dda42c92ba23e1b0aee7c4f5a
Lazaros Inepologlou <li@tcpartners.fr>
2013-01-29 15:48:17 +0100

    bug...

--- src/Actions/Progress/ActionCancelProgress.php
+++ src/Actions/Progress/ActionCancelProgress.php
@@ -4,17 +4,17 @@ class ActionCancelProgress extends Action{
 	public function GetDefaultMode(){ return Action::MODE_HTML_FRAGMENT; }
 	public function IsPermitted(){ return true; }
 
-	private $name;
-	public function __construct($name){$this->name = $name; parent::__construct();}
-	public function GetUrlArgs(){ return array('name'=>$this->name); }
+	private $progress_name;
+	public function __construct($name){$this->progress_name = $name; parent::__construct();}
+	public function GetUrlArgs(){ return array('name'=>$this->progress_name); }
 	public static function Make(){ return new static(Http::$GET['name']->AsString()); }
 
 	public function Render(){
 
 		Progress::Cancel();
 		echo Js::BEGIN;
-		echo "$(".new Js($this->name.'_table').").hide();";
-		echo "$(".new Js($this->name.'_cancelling').").show();";
+		echo "$(".new Js($this->progress_name.'_table').").hide();";
+		echo "$(".new Js($this->progress_name.'_cancelling').").show();";
 		echo Js::END;
 
 	}
--- src/Actions/Progress/ActionUpdateProgress.php
+++ src/Actions/Progress/ActionUpdateProgress.php
@@ -6,9 +6,9 @@ class ActionUpdateProgress extends Action{
 
 	const INTERVAL = 1000;
 
-	private $name;
-	public function __construct($name){$this->name = $name; parent::__construct();}
-	public function GetUrlArgs(){ return array('name'=>$this->name); }
+	private $progress_name;
+	public function __construct($name){$this->progress_name = $name; parent::__construct();}
+	public function GetUrlArgs(){ return array('name'=>$this->progress_name); }
 	public static function Make(){ return new static(Http::$GET['name']->AsString()); }
 
 	public function Render(){
@@ -18,31 +18,33 @@ class ActionUpdateProgress extends Action{
 		$a = Progress::GetLogEntries();
 		if (count($a) > 0){
 			foreach($a as $index => $entry){
-				echo '<div class="progress-message" id="'.$this->name.'_log_'.$index.'">';
+				echo '<div class="progress-message" id="'.$this->progress_name.'_log_'.$index.'">';
 				echo $entry;
 				echo '</div>';
 			}
-			echo Js::BEGIN;
-			echo "$(".new Js($this->name.'_log').").scrollTop = $(".new Js($this->name.'_log_'.$index).").cumulativeOffset().top-$(".new Js($this->name.'_log').").cumulativeOffset().top-5;";
-			echo Js::END;
+			if (isset($index)) {
+				echo Js::BEGIN;
+				echo "$(".new Js($this->progress_name.'_log').").scrollTop = $(".new Js($this->progress_name.'_log_'.$index).").cumulativeOffset().top-$(".new Js($this->progress_name.'_log').").cumulativeOffset().top-5;";
+				echo Js::END;
+			}
 		}
 
 		echo Js::BEGIN;
 		if ($progress < 0){
-			echo "$(".new Js($this->name.'_progress').").hide();";
-			echo "$(".new Js($this->name.'_progress_bar').").style.width = ".new Js('0').";";
+			echo "$(".new Js($this->progress_name.'_progress').").hide();";
+			echo "$(".new Js($this->progress_name.'_progress_bar').").style.width = ".new Js('0').";";
 		}
 		else {
-			echo "$(".new Js($this->name.'_progress').").update(".new Js(floor($progress*100).'%').");";
-			echo "$(".new Js($this->name.'_progress_bar').").style.width = ".new Js(floor($progress*100).'%').";";
+			echo "$(".new Js($this->progress_name.'_progress').").update(".new Js(floor($progress*100).'%').");";
+			echo "$(".new Js($this->progress_name.'_progress_bar').").style.width = ".new Js(floor($progress*100).'%').";";
 		}
 
 		if(!$finished)
-			echo "setTimeout(function(){new Ajax.Updater(".new Js($this->name.'_log').",".new Js($this).",{method:'get',encoding:Oxygen.Encoding,evalScripts:true,insertion:'bottom'});},".self::INTERVAL.");";
+			echo "setTimeout(function(){new Ajax.Updater(".new Js($this->progress_name.'_log').",".new Js($this).",{method:'get',encoding:Oxygen.Encoding,evalScripts:true,insertion:'bottom'});},".self::INTERVAL.");";
 		else{
-			echo "\$(".new Js($this->name.'_cancelling').").hide();";
-			echo "\$(".new Js($this->name.'_table').").hide();";
-			echo "\$(".new Js($this->name.'_actual').").show();";
+			echo "\$(".new Js($this->progress_name.'_cancelling').").hide();";
+			echo "\$(".new Js($this->progress_name.'_table').").hide();";
+			echo "\$(".new Js($this->progress_name.'_actual').").show();";
 			Progress::Clear();
 		}
 		echo Js::END;
