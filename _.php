0c61b6cd6805b216b8d5f52d99344f92559c5b29
Lazaros Inepologlou <li@tcpartners.fr>
2013-05-15 13:28:54 +0200

    ...

--- _.php
+++ _.php
@@ -78,10 +78,12 @@ function id( $iid_or_siid_or_null ){
 function iid( $id_or_item_or_null ){
 	if ($id_or_item_or_null instanceof ID) return $id_or_item_or_null->AsInt();
 	if ($id_or_item_or_null instanceof XItem) return $id_or_item_or_null->id->AsInt();
+	if (is_int($id_or_item_or_null)) return $id_or_item_or_null;
 	return null;
 }
 function hex( $id_or_item_or_null ){
 	if ($id_or_item_or_null instanceof ID) return $id_or_item_or_null->AsHex();
 	if ($id_or_item_or_null instanceof XItem) return $id_or_item_or_null->id->AsHex();
+	if (is_int($id_or_item_or_null)) return (new ID($id_or_item_or_null))->AsHex();
 	return null;
 }
