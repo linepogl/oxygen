0944cfc730a5df1cb90b8b8ee9420ee5b6e2fcf0
Lazaros Inepologlou <li@tcpartners.fr>
2013-07-08 11:23:44 +0200

    cleaning up the database

--- _upgrade.php
+++ _upgrade.php
@@ -1,56 +1,19 @@
 <?php
 
-Database::SetPatchingSystem('oxygen','oxy_settings');
+Database::SetPatchingSystem('oxygen','oxy');
 Database::AddPatcher('v','Version');
 
 if (Database::BeginPatchingSystem()) {
-	Database::ExecuteCreateStandardTable('oxy_settings','Version',Sql::Integer);
-	Database::ExecuteInsert('oxy_settings','id',new ID(0));
+	try { Database::ExecuteDropTable('oxy_settings'); } catch (Exception $ex){}
+	try { Database::ExecuteDropTable('oxy_ids'); } catch (Exception $ex){}
+	try { Database::ExecuteDropTable('oxy_emails'); } catch (Exception $ex){}
+	try { Database::ExecuteDropTable('oxy_local_lemmas'); } catch (Exception $ex){}
+	try { Database::ExecuteDropTable('oxy_unhandled_exceptions'); } catch (Exception $ex){}
+	Database::ExecuteCreateStandardTable('oxy','Version',Sql::Integer);
+	Database::ExecuteInsert('oxy','id',new ID(0));
 }
-
 if (Database::BeginPatch('v',1,'Initial tables')) {
 	Database::ExecuteCreateTable('oxy_ids','TableName',Sql::String100 ,'LastID',Sql::ID);
 	Database::ExecuteAddPrimaryKey('oxy_ids','TableName');
-	Database::ExecuteCreateStandardTable('oxy_emails'
-		,'XFrom',Sql::String255
-		,'Rcpt',Sql::Text
-		,'Subject',Sql::String255
-		,'Body',Sql::Text
-		,'DateSent',Sql::DateTime
-		);
-	Database::ExecuteCreateStandardTable('oxy_local_lemmas'
-		,'Name',Sql::String255
-		,'Overlap',Sql::Text
-		);
-
-	Database::ExecuteCreateStandardTable('oxy_unhandled_exceptions'
-		,'ActionName',Sql::String255
-		,'ActionLine',Sql::Integer
-		,'ExceptionClassName',Sql::String255
-		,'ExceptionMessage',Sql::Text
-		,'ExceptionFilename',Sql::String255
-		,'ExceptionLine',Sql::Integer
-		,'ExceptionTrace',Sql::Text
-		,'DateOccured',Sql::DateTime
-		,'Href',Sql::Text
-		,'IsPostback',Sql::Boolean
-		);
-	Database::ApplyPatch();
-}
-if (Database::BeginPatch('v',10,'Oxygen 1.10')) {
-	Database::ApplyPatch();
-}
-
-if (Database::BeginPatch('v',11,'E-mails')) {
-	Database::ExecuteDropTable('oxy_emails');
-	Database::ExecuteDelete('oxy_ids',new SqlIden('TableName').'='.new Sql('oxy_emails'));
-	Database::ApplyPatch();
-}
-if (Database::BeginPatch('v',12,'Remove unhandled exceptions')) {
-	Database::ExecuteDropTable('oxy_unhandled_exceptions');
-	Database::ApplyPatch();
-}
-if (Database::BeginPatch('v',13,'Remove local lemmata')) {
-	Database::ExecuteDropTable('oxy_local_lemmas');
 	Database::ApplyPatch();
 }
