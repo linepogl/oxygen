0beb1366fe704c3c3482a32325900e2fd55cfa67
Lazaros Inepologlou <li@tcpartners.fr>
2014-03-24 17:14:23 +0100

    ...

--- src/Oxygen.php
+++ src/Oxygen.php
@@ -832,6 +832,7 @@ class Oxygen {
 		ob_start();
 		echo '<meta http-equiv="Content-type" content="'.Oxygen::GetContentType().';charset='.Oxygen::GetCharset().'" />';
 		echo '<base href="'.new Html(Oxygen::GetHrefBaseFull()).'" />';
+//		echo '<meta name="viewport" content="width=device-width,initial-scale=1.0">';
 
 		echo Js::BEGIN;
 		if (self::$window_scoping_enabled){
