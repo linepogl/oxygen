6d3e6139c76c1333cfa0514613f1d7fba8a2eb95
Lazaros Inepologlou <li@tcpartners.fr>
2015-06-02 16:35:45 +0200

    ...

--- src/TypeSystem/ImportExport/Export/Url.php
+++ src/TypeSystem/ImportExport/Export/Url.php
@@ -11,6 +11,13 @@ final class Url extends ExportValue {
 		return MetaUrl::Type();
 	}
 
+	public static function AppendParams($url,$params) {
+		$r = $url;
+		$i = 0;
+		foreach ($params as $key => $value)
+			$r .= ($i++===0 || strpos($url,'?')<0 ? '?' : '&') . new Url($key) . '=' . new Url($value);
+		return $r;
+	}
 }
 
 
