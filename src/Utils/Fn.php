f122f5728a5f8082de5d130a2e833d3e98f276ee
Lazaros Inepologlou <li@tcpartners.fr>
2015-09-22 10:57:06 +0200

    Fn

---
+++ src/Utils/Fn.php
@@ -0,0 +1,20 @@
+<?php
+
+class Fn {
+
+	public static function Insist($function,$times = 0,$sleep = 1) {
+		$i = 0;
+		while(true) {
+			try {
+				return $function();
+			}
+			catch (Exception $ex) {
+				if ($times > 0) if (++$i === $times) throw $ex;
+				if ($sleep > 0) sleep($sleep);
+			}
+		}
+		return null; // should never reach this
+	}
+
+
+}
